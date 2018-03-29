0dc3987b350cfa5ef83e620cf2ed4c29268ee2da
Haimeng Zhang <hzhang1@macalester.edu>
2018-03-28 23:42:30 -0500

    hashmap

---
+++ lab16-HashpMap-Phonebook-Fib/Person.java
@@ -0,0 +1,36 @@
+public class Person {
+
+	private String myName;
+
+	public Person(String name) {
+		this.myName = name;
+	}
+
+	// return a String representation of the Person object
+	public String toString() {
+		return myName;
+	}
+
+	// Change the name of the person
+	public void changeName(String newName) {
+		this.myName = newName;
+	}
+	
+	// TODO add additional methods
+
+	@Override
+
+    public int hashCode () {
+	    return myName.hashCode();
+	}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        return myName != null ? myName.equals(person.myName) : person.myName == null;
+    }
+}
